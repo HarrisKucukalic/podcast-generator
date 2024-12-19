b5a2ad85c5f98354f67a9dd7cb9976741f01d813
HarrisKucukalic <148285864+HarrisKucukalic@users.noreply.github.com>
2024-12-19 04:45:34 +0000

    modified:   Dockerfile modified:   entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ FROM ubuntu:latest
 RUN apt-get update && apt-get install -y \
     python3.10 \
     python3-pip \
-    git
+    git && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
 
 RUN pip3 install PyYAML
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 echo "=============="
+ENV GITHUB_ACTOR=actor-placeholder
+ENV INPUT_EMAIL=email-placeholder@example.com
 
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
+python3 /usr/bin/feed.py || { echo "Python script failed"; exit 1; }
+
 python3 /usr/bin/feed.py
 
-git add -A && git commit -m "Update Feed"
-git push --set-upstream origin main
+git add -A
+git commit -m "Update Feed" || echo "Nothing to commit"
+
+git push --set-upstream origin main || { echo "Git push failed"; exit 1; }
 
 echo "=============="
